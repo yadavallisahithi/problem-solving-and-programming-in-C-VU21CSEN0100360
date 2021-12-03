17aa5b8b5d501dc026590c29eef3dcd9fdeaf28f
yadavallisahithi <93913060+yadavallisahithi@users.noreply.github.com>
2021-12-03 10:28:44 +0530

    Add files via upload

---
+++ covertion.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+main()
+{
+	int a,b;
+	
+	printf("enter a value for hour :");
+	
+	scanf("%d",&a);
+	
+	b=(a*3600);
+	
+	printf("%d in seconds ",b);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
