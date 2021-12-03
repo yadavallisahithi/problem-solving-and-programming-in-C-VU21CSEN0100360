609c03402cc0dd192c17ee945e269828c06ce947
yadavallisahithi <93913060+yadavallisahithi@users.noreply.github.com>
2021-12-03 10:26:10 +0530

    Add files via upload

---
+++ angle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  int a,b,c;
+  printf("enter the value of a :");
+  scanf("%d",&a);
+  printf("enter the value of b :");
+  scanf("%d",&b);
+  c=180-(a+b);
+  printf("the value of third angle=%d", c );
+  return 0;
+}
+   
+
---
+++ convertion.cc
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c,d,e,f;
+	\\time
+	printf("enter a value for hour :");
+	scanf("%d",&a);
+	b=(a*3600);
+	printf("%d in seconds ",b);
+	
+	printf("enter value in km : ");
+	scanf("%d",&c);
+	
+	d=c*1000;
+	printf("%d in meters",d);
+	
+	
+	
+	
+	
+}
+
