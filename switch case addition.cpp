a33c7a5365b6f5fb23637bc4cd9e1b8b3b2d3577
yadavallisahithi <93913060+yadavallisahithi@users.noreply.github.com>
2021-12-03 10:34:27 +0530

    Add files via upload

---
+++ switch case addition.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,ch;
+    printf("enter the value of a,b :");
+    scanf("%d%d",&a,&b);
+    printf("enter the value of choice :");
+    scanf("%d",&ch);
+    switch(ch)
+    {
+        case1:
+        printf("addition of two numbers is%d",a+b);
+        break;
+        default:
+        printf("enter your correct choice");
+        
+    }
+
+   
+    
+    return 0;
+}
